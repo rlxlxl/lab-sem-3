7b61f6c6a0a869ad9f81c14c720b719cbde87068
rlxlxl <slavavik1337@mail.ru>
2025-10-07 00:38:34 +0700

    Добавлен enum в main

--- лаб1/src/main.cpp
+++ лаб1/src/main.cpp
@@ -1,12 +1,46 @@
+/*#include "Queue.hpp"
+#include "Stack.hpp"
 #include "Array.hpp"
-#include "ForwardList.hpp"
 #include "DoublyList.hpp"
-#include <Stack.hpp>
-int main() {
-    Stack<int> test = create_stack<int>();
-    print_stack<int>(test);
-    push_stack(test, 12);
-    push_stack(test, 10);
-    pop_stack(test);
-    print_stack<int>(test);
+#include "ForwardList.hpp"*/
+#include "Array.hpp"
+#include "SystemWork.hpp"
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+enum commands {
+    MPUSH = 1,
+    MDEL = 2,
+    MGET = 3,
+    FPUSH = 4,
+    FDEL = 5,
+    FGET = 6,
+    LPUSH = 7,
+    LDEL = 8,
+    LGET = 9,
+    QPUSH = 10,
+    QPOP = 11,
+    QGET = 12,
+    SPUSH = 13,
+    SPOP = 14,
+    SGET = 15,
+    EXIT = 0
+};
+
+int main(int argc, char* argv[]) {
+    bool short_commands = false;
+    string path = string(argv[2]);
+    Array<string> lines = write_in_dbms(path);
+    Array<string> commands = read_command(argv[3]);
+    
+    if (commands.data[0] == "QPOP" || commands.data[0] == "SPOP") {
+        short_commands = true;
+    }
+
+    if (short_commands) {
+    }
 }
+
