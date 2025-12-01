45eacb8dece044aa30cdbb8dcf4e26104819d09e
rlxlxl <slavavik1337@mail.ru>
2025-12-01 22:24:17 +0700

    лаба 5

---
+++ лаб5/build/CMakeFiles/CodeToHtml.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/slavavikentev/Desktop/учеба/семестр 3/прога/lab-sem-3/лаб5/src/BasicConverter.cpp" "CMakeFiles/CodeToHtml.dir/src/BasicConverter.cpp.o" "gcc" "CMakeFiles/CodeToHtml.dir/src/BasicConverter.cpp.o.d"
+  "/Users/slavavikentev/Desktop/учеба/семестр 3/прога/lab-sem-3/лаб5/src/SyntaxHighlightDecorator.cpp" "CMakeFiles/CodeToHtml.dir/src/SyntaxHighlightDecorator.cpp.o" "gcc" "CMakeFiles/CodeToHtml.dir/src/SyntaxHighlightDecorator.cpp.o.d"
+  "/Users/slavavikentev/Desktop/учеба/семестр 3/прога/lab-sem-3/лаб5/src/main.cpp" "CMakeFiles/CodeToHtml.dir/src/main.cpp.o" "gcc" "CMakeFiles/CodeToHtml.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
